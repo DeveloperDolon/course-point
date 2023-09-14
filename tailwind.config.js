cd040be2f990023972bb89e3566a887531681302
Dolon Roy <dolonr718@gmail.com>
2023-09-14 18:52:30 +0600

    starting commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+export const content = [
+  "./index.html",
+  "./src/**/*.{js,jsx,ts,tsx}",
+];
+export const theme = {};
+export const plugins = [
+  // eslint-disable-next-line no-undef
+  require("daisyui"),
+];
